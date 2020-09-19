8c83afbfc39b45c6c53ab406222c571eaa55e67d
史春阳 <shichunyang02@meituan.com>
2020-09-19 21:15:41 +0800

    WebConfig.java

--- src/main/java/com/scy/web/config/WebConfig.java
+++ src/main/java/com/scy/web/config/WebConfig.java
@@ -1,6 +1,14 @@
 package com.scy.web.config;
 
+import com.scy.core.enums.OrderEnum;
+import com.scy.web.filter.WebFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+import java.util.Collections;
 
 /**
  * WebConfig
@@ -13,5 +21,24 @@ public class WebConfig {
     @Bean
     public CloseApplicationListener closeApplicationListener() {
         return new CloseApplicationListener();
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // 单个文件最大
+        factory.setMaxFileSize(DataSize.parse("5MB"));
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.parse("20MB"));
+        return factory.createMultipartConfig();
+    }
+
+    @Bean
+    public FilterRegistrationBean<WebFilter> webFilter() {
+        FilterRegistrationBean<WebFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new WebFilter());
+        filterRegistrationBean.setUrlPatterns(Collections.singletonList("/*"));
+        filterRegistrationBean.setOrder(OrderEnum.SYSTEM_START.getOrder());
+        return filterRegistrationBean;
     }
 }
