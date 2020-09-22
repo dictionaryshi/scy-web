221897a7ee1232b230d1603e55fc57d6695b0b5f
史春阳 <shichunyang02@meituan.com>
2020-09-22 12:12:04 +0800

    ExceptionHandlerUtil.java

--- src/main/java/com/scy/web/util/ExceptionHandlerUtil.java
+++ src/main/java/com/scy/web/util/ExceptionHandlerUtil.java
@@ -2,6 +2,7 @@ package com.scy.web.util;
 
 import com.scy.core.StringUtil;
 import com.scy.core.enums.ResponseCodeEnum;
+import com.scy.core.exception.BusinessException;
 import com.scy.core.format.MessageUtil;
 import com.scy.core.rest.ResponseResult;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +27,10 @@ public class ExceptionHandlerUtil {
             HttpServletRequest request,
             Throwable throwable
     ) {
+        if (throwable instanceof BusinessException) {
+            return handleBusinessException((BusinessException) throwable);
+        }
+
         ResponseResult<?> result = ResponseResult.error(ResponseCodeEnum.SYSTEM_EXCEPTION.getCode(), throwable.getMessage(), null);
         if (!StringUtil.isEmpty(throwable.getMessage()) && throwable.getMessage().toLowerCase().contains(BROKEN_PIPE)) {
             log.warn(MessageUtil.format(BROKEN_PIPE, throwable, "url", request.getRequestURL().toString()));
@@ -33,5 +38,11 @@ public class ExceptionHandlerUtil {
             log.error(MessageUtil.format("http error", throwable, "url", request.getRequestURL().toString()));
         }
         return result;
+    }
+
+    private static ResponseResult<?> handleBusinessException(BusinessException businessException) {
+        ResponseResult<?> responseResult = ResponseResult.error(businessException.getCode(), businessException.getMessage(), null);
+        log.info(MessageUtil.format("businessException", businessException.getThrowable(), "result", responseResult.toString()));
+        return responseResult;
     }
 }
