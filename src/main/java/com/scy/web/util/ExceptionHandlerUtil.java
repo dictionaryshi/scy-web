31a72dca1ef3df888315f225003ee65197ee40ae
史春阳 <shichunyang02@meituan.com>
2020-09-22 14:05:22 +0800

    ExceptionHandlerUtil.java

--- src/main/java/com/scy/web/util/ExceptionHandlerUtil.java
+++ src/main/java/com/scy/web/util/ExceptionHandlerUtil.java
@@ -6,8 +6,11 @@ import com.scy.core.exception.BusinessException;
 import com.scy.core.format.MessageUtil;
 import com.scy.core.rest.ResponseResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 /**
  * ExceptionHandlerUtil
@@ -31,6 +34,14 @@ public class ExceptionHandlerUtil {
             return handleBusinessException((BusinessException) throwable);
         }
 
+        if (throwable instanceof MissingServletRequestParameterException) {
+            return handleMissingServletRequestParameterException((MissingServletRequestParameterException) throwable);
+        }
+
+        if (throwable instanceof HttpRequestMethodNotSupportedException) {
+            return handleHttpRequestMethodNotSupportedException((HttpRequestMethodNotSupportedException) throwable);
+        }
+
         ResponseResult<?> result = ResponseResult.error(ResponseCodeEnum.SYSTEM_EXCEPTION.getCode(), throwable.getMessage(), null);
         if (!StringUtil.isEmpty(throwable.getMessage()) && throwable.getMessage().toLowerCase().contains(BROKEN_PIPE)) {
             log.warn(MessageUtil.format(BROKEN_PIPE, throwable, "url", request.getRequestURL().toString()));
@@ -38,6 +49,20 @@ public class ExceptionHandlerUtil {
             log.error(MessageUtil.format("http error", throwable, "url", request.getRequestURL().toString()));
         }
         return result;
+    }
+
+    private static ResponseResult<?> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException httpRequestMethodNotSupportedException) {
+        ResponseResult<?> responseResult = ResponseResult.error(ResponseCodeEnum.PARAMS_EXCEPTION.getCode(), MessageUtil.format(httpRequestMethodNotSupportedException.getMessage(),
+                "supportedMethods", Arrays.toString(httpRequestMethodNotSupportedException.getSupportedMethods())), null);
+        log.info(MessageUtil.format("httpRequestMethodNotSupportedException", "result", responseResult.toString()));
+        return responseResult;
+    }
+
+    private static ResponseResult<?> handleMissingServletRequestParameterException(MissingServletRequestParameterException missingServletRequestParameterException) {
+        ResponseResult<?> responseResult = ResponseResult.error(ResponseCodeEnum.PARAMS_EXCEPTION.getCode(), MessageUtil.format("缺少参数",
+                "parameterName", missingServletRequestParameterException.getParameterName(), "parameterType", missingServletRequestParameterException.getParameterType()), null);
+        log.info(MessageUtil.format("missingServletRequestParameterException", "result", responseResult.toString()));
+        return responseResult;
     }
 
     private static ResponseResult<?> handleBusinessException(BusinessException businessException) {
