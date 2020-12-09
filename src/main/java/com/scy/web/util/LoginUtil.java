331b179d6591f0272d851126cc7389806c5a0816
史春阳 <shichunyang02@meituan.com>
2020-12-09 14:57:32 +0800

    LoginUtil.java

--- src/main/java/com/scy/web/util/LoginUtil.java
+++ src/main/java/com/scy/web/util/LoginUtil.java
@@ -1,10 +1,16 @@
 package com.scy.web.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.scy.core.ObjectUtil;
 import com.scy.core.StringUtil;
 import com.scy.core.enums.ResponseCodeEnum;
 import com.scy.core.exception.BusinessException;
+import com.scy.core.format.MessageFormatUtil;
+import com.scy.core.json.JsonUtil;
+import com.scy.core.net.HttpOptions;
+import com.scy.core.net.HttpUtil;
 import com.scy.core.reflect.AnnotationUtil;
+import com.scy.core.rest.ResponseResult;
 import com.scy.core.thread.ThreadLocalUtil;
 import com.scy.web.annotation.LoginCheck;
 import com.scy.web.model.UserTokenBO;
@@ -29,6 +35,8 @@ public class LoginUtil {
 
     public static final String COOKIE_SSO = "SCY_SSO";
 
+    public static final String GET_LOGIN_USER_URL = "http://127.0.0.1:9000/sso/get-login-user?token={0}";
+
     public static String getToken(HttpServletRequest request) {
         // 从请求参数中获取
         String loginToken = request.getParameter(TOKEN);
@@ -52,8 +60,8 @@ public class LoginUtil {
         return (UserTokenBO) ThreadLocalUtil.get(LOGIN_USER);
     }
 
-    public static void setLoginUser(UserTokenBO userBO) {
-        ThreadLocalUtil.put(LOGIN_USER, userBO);
+    public static void setLoginUser(UserTokenBO userTokenBO) {
+        ThreadLocalUtil.put(LOGIN_USER, userTokenBO);
     }
 
     public static void loginCheck(HttpServletRequest request, Method method) {
@@ -69,8 +77,22 @@ public class LoginUtil {
             throw new BusinessException(ResponseCodeEnum.SYSTEM_EXCEPTION.getCode(), "getToken error");
         }
 
-        UserTokenBO userBO = new UserTokenBO();
+        UserTokenBO userTokenBO = getUserTokenByToken(token);
+        if (ObjectUtil.isNull(userTokenBO)) {
+            throw new BusinessException(ResponseCodeEnum.SYSTEM_EXCEPTION.getCode(), errorMessage);
+        }
 
-        setLoginUser(userBO);
+        setLoginUser(userTokenBO);
+    }
+
+    private static UserTokenBO getUserTokenByToken(String token) {
+        String url = MessageFormatUtil.format(GET_LOGIN_USER_URL, token);
+        String json = HttpUtil.get(url, null, HttpOptions.build());
+        ResponseResult<UserTokenBO> responseResult = JsonUtil.json2Object(json, new TypeReference<ResponseResult<UserTokenBO>>() {
+        });
+        if (ObjectUtil.isNull(responseResult)) {
+            return null;
+        }
+        return responseResult.getData();
     }
 }
